d6f5f68af7414e3398680be35c69b0e63f5f00d7
MurylloEx <muryllopimenta@gmail.com>
2021-02-21 19:15:54 -0300

    Correção da questão 2.1

--- exercicios/livro/Cap2/2_1.hs
+++ exercicios/livro/Cap2/2_1.hs
@@ -2,11 +2,11 @@ module Main where
 
     main :: IO ()
     main = do
-        print $ show [1,11,121,1331,14641,161051,1771561]
-        print $ show [1,2,3,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23,25,26,27,29,30,31,33,34,35,37,38,39]
-        print $ show ["AaBB","AbBB","AcBB","AdBB","AeBB","AfBB","AgBB"]
-        print $ show [5,8,11,17,20,26,29,32,38,41]
-        print $ show [1.0,0.5,0.25,0.125,0.0625,0.03125]
-        print $ show [1,10,19,28,37,46,55,64]
-        print $ show [2,4,8,10,12,16,18,22,24,28,30]
-        print $ show ['@','A','C','D','E','G','J','L']
+        print $ show [11 ^ x | x <- [0..6]]
+        print $ show [x | x <- [1..39], x `mod` 4 /= 0]
+        print $ show ['A':x:"BB" | x <- "abcdefg"]
+        print $ show [(3*x) - 1 | x <- [2..11], x `notElem` [5, 8, 12]]
+        print $ show [1.0 / 2^x | x <- [0..5]]
+        print $ show [(9*x) + 1 | x <- [0..6]]
+        print $ show [2*x | x <- [1..15],  x `notElem` [3, 7, 10, 13]]
+        print $ show [x | x <- "@ACDEGHJL"]

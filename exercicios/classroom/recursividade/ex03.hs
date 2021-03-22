5562ddf6957db4cb931c972807566c5a06033bae
MurylloEx <muryllopimenta@gmail.com>
2021-03-22 10:01:52 -0300

    Melhoria ex03.hs em recursividade

--- exercicios/classroom/recursividade/ex03.hs
+++ exercicios/classroom/recursividade/ex03.hs
@@ -1,11 +1,11 @@
 
-(^^^) :: (Num a, Eq a, Ord a) => a -> a -> a
+(^^^) :: (Fractional a, Eq a, Ord a) => a -> a -> a
 (^^^) x 0 = 1
 (^^^) x y 
-    | y < 0 = 0
+    | y < 0 = 1 / (x ^^^ abs y)
     | otherwise = x * (x ^^^ (y-1))
 
 
 main :: IO ()
 main = do
-    print $ 10 ^^^ 2
+    print $ 10 ^^^ (-1)
